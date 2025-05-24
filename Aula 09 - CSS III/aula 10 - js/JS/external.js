b4d48a1307460fcd07d7f6e7d5a0e9b7d506f88a
mauriinacio7 <mauriinacio7@gmail.com>
2025-05-24 11:40:57 -0300

    mauriinacio7

---
+++ Aula 09 - CSS III/aula 10 - js/JS/external.js
@@ -0,0 +1,31 @@
+function teste(){
+    alert("função teste ativada!")
+}
+
+
+let idade = 25; 
+console.log(idade)
+
+let RA = 1111235;
+let nome = 'mauro'
+let sobrenome = 'inacio'
+let familia = 'lucio'
+
+console.log(nome)
+console.log(sobrenome)
+console.log(familia)
+
+
+let nota = 9.50
+console.log(nota)
+
+let verdadeiro = true
+let falso = false
+console.log(true)
+console.log(false)
+
+let vazio = null
+console.log(vazio)
+
+let indefinido
+console.log(indefinido) 
